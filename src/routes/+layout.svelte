52a6bfcd9b9c4024b2b235b30f3bd3ef76482b57
int0p <closer.fz@gmail.com>
2023-04-03 02:41:08 +0900

    css - remove menu drawer(얘가 모바일에선 drawer누르든 말든 보임)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,22 @@
 </script>
 
 <!-- menu drawer  (spectre.css)  -->
-<label for="drawer-control" class="drawer-toggle persistent"></label>
+<!--<label for="drawer-control" class="drawer-toggle persistent"></label>-->
 
-<input type="checkbox" id="drawer-control" class="drawer persistent">
-<div>
-    <label for="drawer-control" class="drawer-close"></label>
-    <a href="#">Home</a>
-    <nav>
-        <a href="#">Category 1</a>
-        <span>Category 2</span>
-        <a href="#" class="sublink-1">Item 2.1</a>
-        <span class="sublink-1">Category 2.2</span>
-        <a href="#" class="sublink-2">Item 2.2.1</a>
-    </nav>
-</div>
+<!--<input type="checkbox" id="drawer-control" class="drawer persistent">-->
+<!--<div>-->
+<!--    <label for="drawer-control" class="drawer-close"></label>-->
+<!--    <a href="#">Home</a>-->
+<!--    <nav>-->
+<!--        <a href="#">Category 1</a>-->
+<!--        <span>Category 2</span>-->
+<!--        <a href="#" class="sublink-1">Item 2.1</a>-->
+<!--        <span class="sublink-1">Category 2.2</span>-->
+<!--        <a href="#" class="sublink-2">Item 2.2.1</a>-->
+<!--    </nav>-->
+<!--</div>-->
 
 <!-- menu tab-->
-
-<!-- basic dropdown button -->
 <div class="dropdown">
     <a href="." class="btn btn-link dropdown-toggle" tabindex="0">
         dropdown menu <i class="icon icon-caret"></i>

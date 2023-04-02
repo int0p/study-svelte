a285f342a03fa3c2bc5c9edaa500fa990401e7f5
int0p <closer.fz@gmail.com>
2023-04-03 02:02:05 +0900

    css style (add mini.css & scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import "../app.scss";
+</script>
+
+<label for="drawer-control" class="drawer-toggle"></label>
+
+<input type="checkbox" id="drawer-control" class="drawer">
+<div>
+    <label for="drawer-control" class="drawer-close"></label>
+    <a href="#">Home</a>
+</div>
+
+<nav>
+    <a href="#">Category 1</a>
+    <span>Category 2</span>
+    <a href="#" class="sublink-1">Item 2.1</a>
+    <span class="sublink-1">Category 2.2</span>
+    <a href="#" class="sublink-2">Item 2.2.1</a>
+</nav>
+
+<slot />
